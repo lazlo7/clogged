b9bd3ef4a184ec410d8d1106555208282fe506ab
Max Smagin <lazlogogalac777@gmail.com>
2024-10-22 20:44:48 +0300

    Added crud for posters

--- clogged/admin/routes.py
+++ clogged/admin/routes.py
@@ -1,8 +1,8 @@
 from clogged.dependencies import get_db
 from clogged.admin.dependencies import verify_admin_key
 from clogged.admin.schemas import PosterModel, PosterRegistrationModel, PosterRegistrationResponseModel
-from clogged.admin.service import add_poster, get_posters
-from fastapi import APIRouter, Request, Depends
+from clogged.admin.service import add_poster, get_poster, get_posters, remove_poster, update_poster_info
+from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -18,11 +18,25 @@ router = APIRouter(
     "/posters",
     description="Returns all registered posters",
     response_model=list[PosterModel],
-    status_code = 200
+    status_code=200
 )
 async def get_all_posters(db: AsyncSession = Depends(get_db)):
     posters = await get_posters(db)
     return posters
+
+
+@router.get(
+    "/poster/{poster_id}",
+    description="Returns poster information by the given poster id",
+    response_model=PosterModel,
+    status_code=200
+)
+async def get_poster_info(
+    poster_id: int,
+    db: AsyncSession = Depends(get_db)
+):
+    poster = await get_poster(poster_id, db)
+    return poster 
 
 
 @router.post(
@@ -36,9 +50,33 @@ async def create_poster(
     db: AsyncSession = Depends(get_db)
 ):
     poster_id = await add_poster(registration_data.username, registration_data.password, db)
-    return PosterRegistrationResponseModel(poster_id=poster_id)    
+    return poster_id    
 
 
-@router.delete("/poster", status_code=201)
-async def delete_poster(r: Request):
-    pass
+@router.delete(
+    "/poster/{poster_id}",
+    description="Unregisters a poster by the given poster id and returns the removed poster", 
+    response_model=PosterModel,
+    status_code=200
+)
+async def delete_poster(
+    poster_id: int,
+    db: AsyncSession = Depends(get_db)
+):
+    poster = await remove_poster(poster_id, db)
+    return poster
+
+
+@router.put(
+    "/poster/{poster_id}",
+    description="Updates poster information by the given poster id",
+    response_model=PosterModel,
+    status_code=200
+)
+async def update_poster(
+    poster_id: int,
+    new_poster_data: PosterRegistrationModel,
+    db: AsyncSession = Depends(get_db)
+):
+    poster = await update_poster_info(poster_id, new_poster_data.username, new_poster_data.password, db)
+    return poster
--- clogged/admin/service.py
+++ clogged/admin/service.py
@@ -2,7 +2,7 @@ from typing import Any
 from fastapi import HTTPException
 from clogged.blog.models import Poster
 from argon2 import PasswordHasher
-from sqlalchemy import select
+from sqlalchemy import delete, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -17,8 +17,21 @@ async def get_posters(db: AsyncSession) -> list[dict[str, Any]]:
     return posters
 
 
-async def add_poster(username: str, password: str, db: AsyncSession) -> int:
-    """Ads a new poster with given username and password to the database and returns their's id."""
+async def get_poster(poster_id: int, db: AsyncSession) -> dict[str, Any]:
+    """
+    Returns poster in the format of: {'id': poster_id, 'username': username} by the given id.
+    Throws an HTTPException with a relevent detail if poster with such id does not exist.
+    """
+    query = select(Poster.id, Poster.username).where(Poster.id == poster_id)
+    result = (await db.execute(query)).first()
+    if result is None:
+        raise HTTPException(status_code=404, detail="Poster with such id does not exist")
+    
+    return {"id": result.id, "username": result.username}
+
+
+async def add_poster(username: str, password: str, db: AsyncSession) -> dict[str, Any]:
+    """Adds a new poster with the given username and password to the database and returns their's id."""
     # Check if such username already exists.
     query = select(Poster).where(Poster.username == username)
     result = await db.execute(query)
@@ -31,4 +44,43 @@ async def add_poster(username: str, password: str, db: AsyncSession) -> int:
     poster = Poster(username=username, credentials=credentials)
     db.add(poster)
     await db.commit()
-    return poster.id
+    return {"poster_id": poster.id}
+
+
+async def remove_poster(poster_id: int, db: AsyncSession) -> dict[str, Any]:
+    """Removes poster by the given poster id from the database and returns back the removed poster."""
+    query = select(Poster).filter(Poster.id == poster_id)
+    poster = (await db.execute(query)).scalar()
+    if poster is None:
+        raise HTTPException(status_code=404, detail="Poster with such id does not exist")
+
+    removed_poster = {
+        "id": poster.id,
+        "username": poster.username
+    }
+
+    await db.delete(poster)
+    await db.commit()
+    return removed_poster
+
+
+async def update_poster_info(
+    poster_id: int, 
+    new_username: str, 
+    new_password: str, 
+    db: AsyncSession
+) -> dict[str, Any]:
+    """Updates poster information by the given poster id and returns the updated poster."""
+    query = select(Poster).filter(Poster.id == poster_id)
+    poster = (await db.execute(query)).scalar()
+    if poster is None:
+        raise HTTPException(status_code=404, detail="Poster with such id does not exist")
+
+    poster.username = new_username
+    poster.credentials = password_hasher.hash(new_password)
+
+    await db.commit()
+    return {
+        "id": poster.id,
+        "username": poster.username
+    }
